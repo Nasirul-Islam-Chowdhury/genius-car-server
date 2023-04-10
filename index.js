fbbbef458bfdd5455d887a078c36e8d59d2e9553
nicchy123 <nicchy321@gmail.com>
2023-04-10 17:20:14 +0600

    basic server created

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors())
+app.use(express.json());
+require("dotenv").config();
+
+app.get('/', (req, res)=>{
+    res.send("running")
+})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bbqqyyb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+app.listen(port, ()=>{
+    console.log("running")
+})
